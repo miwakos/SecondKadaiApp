5bbf1adc98056286d30b659eeee9c79f13fd59c0
miwakos <goma.kabo@gmail.com>
2025-03-09 10:38:55 +0900

    名前入力画面と名前表示画面の追加2

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.inputName = textField.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 }
 
